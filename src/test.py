30102fbc926359a243a6f9d484ac3c9c1be67bf4
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-06-16 18:45:51 -0500

    Updated File

--- src/test.py
+++ src/test.py
@@ -5,7 +5,6 @@ url = 'https://scrapethissite.com/pages/simple/'
 
 # TODO: store country and capitals into array, save to file
 
-
 r = requests.get(url)
 
 soup = BeautifulSoup(r.text,'lxml')
@@ -14,7 +13,13 @@ soup = BeautifulSoup(r.text,'lxml')
 # with open('scrapeThisSite.html','w') as f:
 #     f.write(str(myHTML))
 
-countryNames = [soup.find_all("h3", class_="country-name")]
+countryNames = [i for i in soup.find_all("h3", class_="country-name")]
+# countryNames = [soup.find("div", class_="col-md-4 country")]
 # strippedNames = [i.get_text() for i in countryNames]
 
-splitText = []
+# parseText = [i for i in countryNames.split(" ")]
+
+# print(parseText)
+
+for x in countryNames:
+    print(x.text.strip())
