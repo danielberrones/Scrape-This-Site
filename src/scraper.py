1632dda8bd9483d3789e29f132b4e2429e45cda8
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-06-16 19:10:21 -0500

    Updated File

--- src/scraper.py
+++ src/scraper.py
@@ -5,32 +5,23 @@ url = 'https://scrapethissite.com/pages/simple/'
 
 # TODO: store country and capitals into array, save to file
 
-r = requests.get(url)
+def soupRequest():
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text,'lxml')
 
-soup = BeautifulSoup(r.text,'lxml')
-# myHTML = soup.html
-# with open('scrapeThisSite.html','w') as f:
-#     f.write(str(myHTML))
-countries = [i.text.strip() for i in soup.find_all("h3", class_="country-name")]
-# countryNames = [soup.find("div", class_="col-md-4 country")]
-# strippedNames = [i.get_text() for i in countryNames]
-# country-capital
-capitals = [i.text.strip() for i in soup.find_all("span", class_="country-capital")]
-combined = dict(zip(countries,capitals))
+def getCountryAndCapital():
+    countries = [i.text.strip() for i in soup.find_all("h3", class_="country-name")]
+    capitals = [i.text.strip() for i in soup.find_all("span", class_="country-capital")]
+    return dict(zip(countries,capitals))
 
-def parseDict():
-    # i = 1
-    for i, (k,v) in enumerate(combined.items()):
+def parseDict(countryAndCapital):
+    for i, (k,v) in enumerate(countryAndCapital.items()):
         print(f"{i+1}\n\nCountry: {k}")
         print(f"Capital: {v}\n\n")
 
-parseDict()
+def main():
+    soupRequest()
+    parseDict(getCountryAndCapital())
 
-# print(len(capitals))
-# print(len(countries))
-# parseText = [i for i in countryNames.split(" ")]
-# print(parseText)
-# for x in countryNames:
-#     print(x.text.strip())
-# print(countryNames[7]=="Angola")
 
+main()
